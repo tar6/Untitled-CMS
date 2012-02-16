c536777c882a413e03260255c9fbe0aa306c2fa9
Joshua Filby <js-filby@hotmail.com>
2012-02-16 05:26:11 -0600

    plugins now work

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 include("./initdata.php");
 include(ROOT_PATH . "system/config.php");
 include(ROOT_PATH . "system/modules/Database.class.php");
+include(ROOT_PATH . "system/modules/PluginManager.class.php");
 
 $database = new Database($INFO['sql_host'], $INFO['sql_username'], $INFO['sql_password'], $INFO['sql_database']);
-
-echo $database->count("test");
-
+$pluginManager = new PluginManager($database);
+$pluginManager->init(PLUGINS_PATH);
 exit();
 
 ?>
--- system/modules/PluginManager.class.php
+++ system/modules/PluginManager.class.php
@@ -7,20 +7,59 @@
 * @version	1.0.0
 * @link		{link}
 * 
+* Thanks to Mikelmao for most of this script.
+* @link http://www.mikelmao.com/
 */
 
 class PluginManager {
 
 	static public $DB = NULL;
-	static public $activePlugins = array();
 	static public $pluginDir = NULL;
+	static public $activePlugins = array();
+	static public $pluginList = array();
+	static public $dbPlugins = array();
 	
 	public function __construct(Database $database) {
 		PluginManager::$DB = $database;
 	}
 	
-	static function init() {
+	static function init($pluginDir = NULL) {
+		if (PluginManager::$pluginDir == NULL)
+			PluginManager::$pluginDir = $pluginDir;
 		
+		if (PluginManager::$pluginDir == NULL)
+			die("The plugin directory doesnt exist.");
+		
+		if($dirHandle = opendir(PluginManager::$pluginDir)) {
+			while($file = readdir($dirHandle)) {
+				if(is_dir(PluginManager::$pluginDir . $file) && $file != '.' && $file != '..')
+				array_push(PluginManager::$pluginList, $file);
+			}
+		}
+		
+		$getPlugins = PluginManager::$DB->query("SELECT * FROM `plugins`");
+		while($plug = mysql_fetch_array($getPlugins)) {
+			PluginManager::$dbPlugins[$plug['name']] = $plug['active'];
+		}
+		
+		foreach(PluginManager::$pluginList as $plugin) {
+			if(PluginManager::$dbPlugins[$plugin] == 1)
+			PluginManager::register($plugin);
+		}
+	}
+	
+	static private function register($plugin) {
+		if(file_exists(PluginManager::$pluginDir . $plugin . '/' . $plugin .'.class.php')) {
+			require_once(PluginManager::$pluginDir . $plugin . '/' . $plugin .'.class.php');
+			array_push(PluginManager::$activePlugins, $plugin);
+		}
+	}
+	
+	static function hook($checkpoint) {
+		foreach(PluginManager::$activePlugins as $plugin) {
+			if(@call_user_func(array($plugin, $checkpoint)))
+			throw new Exception('Enable to hook plugin ('. $plugin .') to checkpoint ('. $checkpoint .')');
+		}
 	}
 	
 }
