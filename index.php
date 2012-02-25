9fdcf4a989298789516cfdd1a87fd16fa4330479
Joshua Filby <js-filby@hotmail.com>
2012-02-25 00:40:46 -0600

    basic(horrible) theme and page manager added

--- index.php
+++ index.php
@@ -13,10 +13,14 @@ include("./initdata.php");
 include(ROOT_PATH . "system/config.php");
 include(ROOT_PATH . "system/modules/Database.class.php");
 include(ROOT_PATH . "system/modules/PluginManager.class.php");
-
-$database = new Database($INFO['sql_host'], $INFO['sql_username'], $INFO['sql_password'], $INFO['sql_database']);
-$pluginManager = new PluginManager($database);
+include(ROOT_PATH . "system/modules/StyleManager.class.php");
+include(ROOT_PATH . "system/modules/PageManager.class.php");
+$database		= new Database($INFO['sql_host'], $INFO['sql_username'], $INFO['sql_password'], $INFO['sql_database']);
+$pluginManager	= new PluginManager($database);
+$styleManager	= new StyleManager($database, STYLE_PATH);
+$pageManager	= new PageManager($_GET['page'], ROOT_PATH);
 $pluginManager->init(PLUGINS_PATH);
+include($pageManager->page);
 $database->close();
 exit();
 
